363352ff347df147add40ee30f76065f7203ecbb
Ernad Husremović <hernad@bring.out.ba>
2022-11-17 16:32:07 +0100

    Redovni Rad, Redovni rad bez TO

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from . import work_type
 from . import payslip_worked_days_timesheet
+from . import account_analytic_line
--- models/work_type.py
+++ models/work_type.py
@@ -8,24 +8,6 @@ class TimesheetsWorkType(models.Model):
 
     name = fields.Char("Work Type", required=True, translate=True)
     code = fields.Char("Code", required=True)
+    food_included = fields.Boolean("Food included?", required=True, default=True)
 
 
-class AccountAnalyticLine(models.Model):
-    _inherit = "account.analytic.line"
-
-    work_type_id = fields.Many2one(string="Work Type",
-                                   comodel_name="hr.timesheet.work_type",
-                                   help="Odaberi vrstu rada")
-    #worked_days_id = fields.Many2one(string="Payslip",
-    #                                 comodel_name="hr.payslip.worked_days")
-
-    worked_days_ids = fields.Many2many(
-       string="Worked days",
-       comodel_name="hr.payslip.worked_days",
-       relation="hr_payslip_analytic_rel",
-       # model records
-       column1="analytic_line_id",
-       # related model records
-       column2="worked_days_id",
-       inverse_name="id"
-    )
